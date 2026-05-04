45c86ab9efcc6a6de660af8db34b557755b466ea
정은진 <eunjin02001004@gmail.com>
2026-05-04 10:07:28 +0900

    springInitialSetting

---
+++ team01web/team01web/src/main/java/com/example/team01web/Team01webApplication.java
@@ -0,0 +1,13 @@
+package com.example.team01web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team01webApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team01webApplication.class, args);
+	}
+
+}
---
+++ team01web/team01web/src/test/java/com/example/team01web/Team01webApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.team01web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team01webApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
