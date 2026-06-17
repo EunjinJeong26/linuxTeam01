8a023314f9960695a44a218a526e8531bdf2368f
정은진 <eunjin02001004@gmail.com>
2026-06-18 00:00:20 +0900

    webConfig 추가

---
+++ linuxTeam01web/src/main/java/com/example/linuxTeam01web/config/webConfig.java
@@ -0,0 +1,15 @@
+package com.example.linuxTeam01web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class webConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173") // 프론트엔드 주소
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
